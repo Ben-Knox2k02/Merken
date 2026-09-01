3f5ef533a6662eef00678d2061cdd48caceb43b7
Ben Knox <ben.knox@okstate.edu>
2026-09-01 16:42:44 -0500

    Add initial implementation of Merken.cpp

---
+++ src/Merken.cpp
@@ -0,0 +1,8 @@
+#include <wx/wxprec.h>	// For compilers that support precompilation, includes "wx/wx.h".
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+#include <wx/spinctrl.h>
+#include "mainframe.h"
+#include "app.h"
+
