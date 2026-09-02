ac01798a7fe0dadb5efadf7400167e8a5b8bf611
Ben Knox <ben.knox@okstate.edu>
2026-09-01 21:27:36 -0500

    Refactor MainFrame to add new menus and panels

--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -1,62 +1,111 @@
-#include "mainframe.h"
 #include <wx/wx.h>
 #include <vector>
 #include <string>
 #include <filesystem>
 #include <fstream>
 #include <algorithm>
+#include "mainframe.h"
+#include "card_list_panel.h"
+#include "deck_panel.h"
 
 const int SCREEN_WIDTH = 1920;
 const int SCREEN_HEIGHT = 1080;
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE) {
-	InitWidgets();
-	BindEventHandlers();
-}
+	//=========== INITIALIZE WIDGETS ===================================== INITIALIZE WIDGETS ================================
+	
+	this->menuBar = new wxMenuBar;						// MENU BAR
+	 
+	this->fileMenu = new wxMenu;						// FILE MENU
+	this->fileMenu->Append(wxID_NEW, "New");		
+	this->fileMenu->Append(wxID_OPEN, "Open");
+	this->fileMenu->Append(wxID_SAVE, "Save");
+	this->fileMenu->AppendSeparator();
+	this->fileMenu->Append(wxID_EXIT, "Exit");
+	 
+	this->editMenu = new wxMenu;						// EDIT MENU
+	this->editMenu->Append(wxID_CUT, "Cut");
+	this->editMenu->Append(wxID_COPY, "Copy");
+	this->editMenu->Append(wxID_PASTE, "Paste");
+	 
+	this->studyMenu = new wxMenu;						// STUDY MENU
+	 
+	this->calendarMenu = new wxMenu;					// CALENDAR MENU
+	 
+	this->AIMenu = new wxMenu;							// AI MENU
+	 
+	this->helpMenu = new wxMenu;						// HELP MENU
+	this->helpMenu->Append(wxID_ABOUT, "About");
+	 
+	this->menuBar->Append(fileMenu, "File");			// ATTACH MENUS
+	this->menuBar->Append(editMenu, "Edit");
+	this->menuBar->Append(studyMenu, "Study");
+	this->menuBar->Append(AIMenu, "AI");
+	this->menuBar->Append(helpMenu, "Help");
+	 
+	this->SetMenuBar(this->menuBar);					// ATTACH MENU BAR / STATUS BAR
+	this->CreateStatusBar();
+	
+	this->rootSizer = new wxBoxSizer(wxHORIZONTAL);
+	
+	this->deckPanel = new DeckPanel(this);
 
-void MainFrame::InitWidgets() {
-	 this->panel = new wxPanel(this);
-	 
-	 this->menuBar = new wxMenuBar;					// MENU BAR
-	 
-	 this->fileMenu = new wxMenu;						// FILE MENU
-	 this->fileMenu->Append(wxID_NEW, "New");		
-	 this->fileMenu->Append(wxID_OPEN, "Open");
-	 this->fileMenu->Append(wxID_SAVE, "Save");
-	 this->fileMenu->AppendSeparator();
-	 this->fileMenu->Append(wxID_EXIT, "Exit");
-	 
-	 this->helpMenu = new wxMenu;						// HELP MENU
-	 this->helpMenu->Append(wxID_ABOUT, "About");
-	 
-	 this->menuBar->Append(fileMenu, "File");			// ATTACH MENUS
-	 this->menuBar->Append(helpMenu, "Help");
-	 
-	 this->SetMenuBar(this->menuBar);					// ATTACH MENU BAR / STATUS BAR
-	 this->CreateStatusBar();
-}
-
-void MainFrame::BindEventHandlers() {
-	this->Bind(wxEVT_MENU, &MainFrame::OnNew, this, wxID_NEW);			// MENU EVENTS
+	this->activePanel = new wxPanel(this, wxID_ANY);
+	this->activePanel->SetBackgroundColour(*wxWHITE);
+	this->SwapCurrentPanel(new CardListPanel(this->activePanel, 0));
+	
+	this->rootSizer->Add(deckPanel, 0, wxEXPAND | wxALL, 0);
+	this->rootSizer->Add(activePanel, 1, wxEXPAND | wxALL, 0);
+	
+	this->SetSizer(this->rootSizer);
+	this->Layout();
+	
+	//========= BIND EVENT HANDLERS ==================================== BIND EVENT HANDLERS =================================
+	
+	this->Bind(wxEVT_MENU, &MainFrame::OnNew, this, wxID_NEW);					// MENU EVENTS
 	this->Bind(wxEVT_MENU, &MainFrame::OnOpen, this, wxID_OPEN);
 	this->Bind(wxEVT_MENU, &MainFrame::OnSave, this, wxID_SAVE);
 	this->Bind(wxEVT_MENU, &MainFrame::OnAbout, this, wxID_ABOUT);
 	this->Bind(wxEVT_MENU, &MainFrame::OnExit, this, wxID_EXIT);
 
-	this->Bind(wxEVT_SIZE, &MainFrame::OnWindowResized, this);			// WINDOW EVENTS
+	this->Bind(wxEVT_SIZE, &MainFrame::OnWindowResized, this);					// WINDOW EVENTS
+	this->Bind(wxEVT_ENTER_WINDOW, &MainFrame::OnMouseEvent, this);
+	this->Bind(wxEVT_LEAVE_WINDOW, &MainFrame::OnMouseEvent, this);
 	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnWindowClosed, this);
-	
-	this->panel->Bind(wxEVT_LEFT_DOWN,    &MainFrame::OnMouseEvent, this);		// MOUSE EVENTS
-	this->panel->Bind(wxEVT_LEFT_UP,      &MainFrame::OnMouseEvent, this);
-	this->panel->Bind(wxEVT_RIGHT_DOWN,   &MainFrame::OnMouseEvent, this);
-	this->panel->Bind(wxEVT_RIGHT_UP,     &MainFrame::OnMouseEvent, this);
-	this->panel->Bind(wxEVT_MOTION,       &MainFrame::OnMouseEvent, this);
-	this->panel->Bind(wxEVT_MOUSEWHEEL,   &MainFrame::OnMouseEvent, this);
-	this->panel->Bind(wxEVT_ENTER_WINDOW, &MainFrame::OnMouseEvent, this);
-	this->panel->Bind(wxEVT_LEAVE_WINDOW, &MainFrame::OnMouseEvent, this);
+}
 
-	this->panel->Bind(wxEVT_KEY_DOWN, &MainFrame::OnKeyEvent, this);				// KEYBOARD EVENTS
-	this->panel->SetFocus();
+void MainFrame::SwapCurrentPanel(wxPanel* newPanel) {
+	if(this->currentPanel != NULL) { 
+		this->currentPanel->Destroy();
+		this->currentPanel = NULL;
+	}
+	
+	this->activePanel->SetSizer(NULL);
+	//this->activePanel->DestroyChildren();
+	
+	this->currentPanel = newPanel;
+	this->currentPanel->SetBackgroundStyle(wxBG_STYLE_PAINT);
+    this->currentPanel->SetBackgroundColour(*wxWHITE);
+	
+	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+	sizer->Add(this->currentPanel, 1, wxEXPAND);
+	
+	this->activePanel->SetSizer(sizer);
+	this->activePanel->Layout();
+	
+	this->currentPanel->Bind(wxEVT_KEY_DOWN, &MainFrame::OnKeyEvent, this);
+	
+	for(wxWindow* child : this->currentPanel->GetChildren()) {
+		child->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnMouseEvent, this);
+        child->Bind(wxEVT_LEFT_UP, &MainFrame::OnMouseEvent, this);
+        child->Bind(wxEVT_RIGHT_DOWN, &MainFrame::OnMouseEvent, this);
+        child->Bind(wxEVT_RIGHT_UP, &MainFrame::OnMouseEvent, this);
+        child->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
+        child->Bind(wxEVT_MOUSEWHEEL, &MainFrame::OnMouseEvent, this);
+        child->Bind(wxEVT_KEY_DOWN, &MainFrame::OnKeyEvent, this);
+	}
+	
+	this->currentPanel->SetFocus();
 }
 
 void MainFrame::OnNew(wxCommandEvent& event) {
