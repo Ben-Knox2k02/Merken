da7ed11fe229223909269d62abb9fd224eccf5e3
Ben Knox <ben.knox@okstate.edu>
2026-09-10 23:56:24 -0500

    Refactor DeckPanel methods and add Deck struct

--- include/deck_panel.h
+++ include/deck_panel.h
@@ -1,25 +1,42 @@
 #ifndef DECK_PANEL_H
 #define DECK_PANEL_H
 
+#include "dialog.h"
+#include <vector>
 #include <wx/wx.h>
 #include <wx/dataview.h>
+#include <wx/dcbuffer.h>
+
+struct Deck {
+	int ID;
+	wxString name;
+	wxString description;
+};
 
 class DeckPanel : public wxPanel {
 	public:
 		DeckPanel(wxWindow* parent);
 		
+		std::vector<Deck> decks;
+		Deck* activeDeck;
+		
 		wxBoxSizer* rootSizer;
 		wxBoxSizer* buttonSizer;
 		wxStaticText* header;
 		wxDataViewCtrl* deckList;
+		wxDataViewListStore* deckViewModel;
 		wxButton* addDeckButton;
 		wxButton* editDeckButton;
 		wxButton* deleteDeckButton;
 		
 		void LoadDecks();
-		void OnAddDeck(wxCommandEvent& event);
-		void OnEditDeck(wxCommandEvent& event);
-		void OnDeleteDeck(wxCommandEvent& event);
+		void Print();
+		int GetSelectedRow() const;
+		
+		void OnPaint(wxPaintEvent& event);
+		void OnAdd(wxCommandEvent& event);
+		void OnEdit(wxCommandEvent& event);
+		void OnDelete(wxCommandEvent& event);
 		void OnDeckActivated(wxDataViewEvent& event);
 };
 
