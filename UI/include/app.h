07e0815bd66470a6ab8a05620cfd243ad85db7ad
Otuyishime <oliviertyishime@gmail.com>
2026-09-11 03:57:15 -0500

    Attempt to speed up windows build

--- UI/include/app.h
+++ UI/include/app.h
@@ -2,6 +2,14 @@
 #include <optional>
 #include "../../DiComposition/composition_root.h"
 
+// Boost.DI's injector is a lambda type (internal linkage). GCC warns when
+// that type is a field of App (external linkage). The injector must stay
+// visible here so UI files can call create<T>().
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsubobject-linkage"
+#endif
+
 class App : public wxApp {
 	public:
 		bool OnInit() override;
@@ -15,3 +23,7 @@ class App : public wxApp {
 		void EnsureInjector();
 		std::optional<decltype(MakeInjector())> injector;
 };
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
