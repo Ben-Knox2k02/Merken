0b2d287dba2ed845782f09f06c7b620c61b9c390
Ben Knox <ben.knox@okstate.edu>
2026-09-10 23:58:33 -0500

    Refactor Card structure and update includes

--- include/card_list_panel.h
+++ include/card_list_panel.h
@@ -1,16 +1,26 @@
 #ifndef CARD_LIST_PANEL_H
 #define CARD_LIST_PANEL_H
 
-#include "card_dialog.h"
+#include "dialog.h"
+#include <vector>
 #include <wx/wx.h>
 #include <wx/dataview.h>
 #include <wx/dcbuffer.h>
+
+struct Card {
+	int ID;
+	wxString front;
+	wxString back;
+	std::vector<wxString> tags;
+};
 
 class CardListPanel : public wxPanel {
 	public:
 		CardListPanel(wxWindow* parent, int deckID);
 		
 		int deckID;
+		std::vector<Card> cards;
+		
 		wxBoxSizer* rootSizer;
 		wxBoxSizer* buttonSizer;
 		wxStaticText* header;
