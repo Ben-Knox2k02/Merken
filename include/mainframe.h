a40d175fe5919e7f75d7db2bf7db9ca01d50ac9c
Ben Knox <ben.knox@okstate.edu>
2026-09-19 15:46:11 -0500

    Add new event handlers and update menu structure

--- include/mainframe.h
+++ include/mainframe.h
@@ -4,6 +4,14 @@
 #include "app.h"
 #include <wx/wx.h>
 
+enum {									// CUSTOM EVENT IDs
+	ID_CARDS = wxID_HIGHEST + 1,
+	ID_STUDY,
+	ID_CALENDAR,
+	ID_AI,
+	ID_SETTINGS,
+};
+
 class MainFrame : public wxFrame {
 	public:
 		MainFrame(const wxString& title);
@@ -18,24 +26,40 @@ class MainFrame : public wxFrame {
 		wxMenuBar* menuBar;
 		wxMenu* fileMenu;
 		wxMenu* editMenu;
-		wxMenu* studyMenu;
-		wxMenu* calendarMenu;
-		wxMenu* AIMenu;
+		wxMenu* searchMenu;
+		wxMenu* viewMenu;
+		wxMenu* toolsMenu;
 		wxMenu* helpMenu;
 		
 		void SwapCurrentPanel(wxPanel* newPanel);
 		
-		void OnNew(wxCommandEvent& event);
+		void OnNew(wxCommandEvent& event);				// FILE
 		void OnOpen(wxCommandEvent& event);
 		void OnSave(wxCommandEvent& event);
-		void OnAbout(wxCommandEvent& event);
 		void OnExit(wxCommandEvent& event);
 		
-		void OnInputEnter(wxCommandEvent& event);
-		void OnWindowResized(wxSizeEvent& event);
-		void OnWindowClosed(wxCloseEvent& event);
-		void OnMouseEvent(wxMouseEvent& event);
+		void OnCut(wxCommandEvent& event);				// EDIT
+		void OnCopy(wxCommandEvent& event);
+		void OnPaste(wxCommandEvent& event);
+		
+		void OnFind(wxCommandEvent& event);			// SEARCH
+		void OnReplace(wxCommandEvent& event);
+		
+		void OnCards(wxCommandEvent& event);			// VIEW
+		void OnStudy(wxCommandEvent& event);
+		void OnCalendar(wxCommandEvent& event);
+		void OnAI(wxCommandEvent& event);
+		
+		void OnSettings(wxCommandEvent& event);		// TOOLS
+		
+		void OnAbout(wxCommandEvent& event);			// HELP
+		
+		void OnMouseEvent(wxMouseEvent& event);			// I/O
 		void OnKeyEvent(wxKeyEvent& event);
+		
+		void OnWindowResized(wxSizeEvent& event);		// WINDOW
+		void OnWindowClosed(wxCloseEvent& event);
+		
 };
 
 #endif
