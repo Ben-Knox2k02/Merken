55e5f60ea925d1ee2d912a80465b8960b5d3aaa5
Ben Knox <ben.knox@okstate.edu>
2026-09-20 23:40:12 -0500

    Add EventDialog with fields for event creation

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -47,3 +47,37 @@ DeckDialog::DeckDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, "Deck", wx
 	this->rootSizer->Add(CreateSeparatedButtonSizer(wxOK | wxCANCEL), 0, wxEXPAND | wxALL, 10);
 	this->SetSizerAndFit(this->rootSizer);
 }
+
+EventDialog::EventDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, "Create Event", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER) {
+	this->SetBackgroundColour(wxColour(*wxGREEN));
+	
+	this->rootSizer = new wxBoxSizer(wxVERTICAL);
+	
+	this->titleText = new wxStaticText(this, wxID_ANY, "Title:");
+	this->titleCtrl = new wxTextCtrl(this, wxID_ANY);
+	this->rootSizer->Add(this->titleText, 0, wxLEFT | wxALL, 5);
+	this->rootSizer->Add(this->titleCtrl, 0, wxRIGHT | wxALL, 5);
+	
+	this->dateText = new wxStaticText(this, wxID_ANY, "Date:");
+	this->dateCtrl = new wxTextCtrl(this, wxID_ANY);
+	this->rootSizer->Add(this->dateText, 0, wxLEFT | wxALL, 5);
+	this->rootSizer->Add(this->dateCtrl, 0, wxRIGHT | wxALL, 5);
+	
+	this->locationText = new wxStaticText(this, wxID_ANY, "Location:");
+	this->locationCtrl = new wxTextCtrl(this, wxID_ANY);
+	this->rootSizer->Add(this->locationText, 0, wxLEFT | wxALL, 5);
+	this->rootSizer->Add(this->locationCtrl, 0, wxRIGHT | wxALL, 5);
+	
+	this->descriptionText = new wxStaticText(this, wxID_ANY, "Description:");
+	this->descriptionCtrl = new wxTextCtrl(this, wxID_ANY);
+	this->rootSizer->Add(this->descriptionText, 0, wxLEFT | wxALL, 5);
+	this->rootSizer->Add(this->descriptionCtrl, 0, wxRIGHT | wxALL, 5);
+	
+	this->reminderText = new wxStaticText(this, wxID_ANY, "Reminder:");
+	this->reminderCtrl = new wxTextCtrl(this, wxID_ANY);
+	this->rootSizer->Add(this->reminderText, 0, wxLEFT | wxALL, 5);
+	this->rootSizer->Add(this->reminderCtrl, 0, wxRIGHT | wxALL, 5);
+	
+	this->rootSizer->Add(CreateSeparatedButtonSizer(wxOK | wxCANCEL), 0, wxEXPAND | wxALL, 10);
+	this->SetSizerAndFit(this->rootSizer);
+}
