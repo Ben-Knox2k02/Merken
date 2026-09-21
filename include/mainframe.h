059ffe19c34252ba41f566cc2d278314db08b708
Ben Knox <ben.knox@okstate.edu>
2026-09-20 23:37:53 -0500

    Refactor MainFrame class and update shortcuts array

--- include/mainframe.h
+++ include/mainframe.h
@@ -5,19 +5,22 @@
 #include "data_structures.h"
 #include <wx/wx.h>
 
+#define NUM_SHORTCUTS 16
+
 class MainFrame : public wxFrame {
 	public:
 		MainFrame(const wxString& title);
-		App& app;								// GLOBAL REFERENCE TO APP
+		App& app;										// GLOBAL REFERENCE TO APP
 		
-		wxPanel* deckPanel;
+		wxPanel* deckPanel;								// LAYOUT PANELS
 		wxPanel* activePanel;
 		wxPanel* currentPanel;
 		
 		wxBoxSizer* rootSizer;
 		
-		wxAcceleratorEntry shortcuts[8];			// KEYBOARD SHORTCUTS
-		wxMenuBar* menuBar;
+		wxAcceleratorEntry shortcuts[NUM_SHORTCUTS];	// KEYBOARD SHORTCUTS
+		
+		wxMenuBar* menuBar;								// MENU
 		wxMenu* fileMenu;
 		wxMenu* editMenu;
 		wxMenu* searchMenu;
@@ -25,18 +28,21 @@ class MainFrame : public wxFrame {
 		wxMenu* toolsMenu;
 		wxMenu* helpMenu;
 		
-		void SwapCurrentPanel(wxPanel* newPanel);
+		void SetCurrentPanel(wxPanel* newPanel);
 		
 		void OnNew(wxCommandEvent& event);				// FILE
 		void OnOpen(wxCommandEvent& event);
 		void OnSave(wxCommandEvent& event);
 		void OnExit(wxCommandEvent& event);
 		
-		void OnCut(wxCommandEvent& event);				// EDIT
+		void OnUndo(wxCommandEvent& event);				// EDIT
+		void OnRedo(wxCommandEvent& event);
+		void OnCut(wxCommandEvent& event);				
 		void OnCopy(wxCommandEvent& event);
 		void OnPaste(wxCommandEvent& event);
+		void OnSelectAll(wxCommandEvent& event);
 		
-		void OnFind(wxCommandEvent& event);			// SEARCH
+		void OnFind(wxCommandEvent& event);				// SEARCH
 		void OnReplace(wxCommandEvent& event);
 		
 		void OnCards(wxCommandEvent& event);			// VIEW
@@ -44,7 +50,7 @@ class MainFrame : public wxFrame {
 		void OnCalendar(wxCommandEvent& event);
 		void OnAI(wxCommandEvent& event);
 		
-		void OnSettings(wxCommandEvent& event);		// TOOLS
+		void OnSettings(wxCommandEvent& event);			// TOOLS
 		
 		void OnAbout(wxCommandEvent& event);			// HELP
 		
